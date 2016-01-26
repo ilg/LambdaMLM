b0cae1da95e280dd451a624e5334a225dfc2f011
Isaac Greenspan <ilg@2718.us>
2016-01-26 01:02:34 -0600

    Virtualenv setup + click for command processing.

--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -10,14 +10,35 @@ from fabric.api import *
 import boto3
 client = boto3.client('lambda', region_name='us-west-2')
 
+basepath = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+
 def update_lambda():
-    basepath = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
     zipfile = os.path.join(basepath, 'lambda.zip')
     codedir = os.path.join(basepath, 'lambda')
-    local('zip --junk-paths "{zipfile}" "{codedir}/"*'.format(zipfile=zipfile, codedir=codedir))
+    libdir = os.path.join(basepath, '.env/lib/python2.7/site-packages')
+    local('''
+        cd "{codedir}"
+        zip --recurse-paths "{zipfile}" * --exclude "*.pyc"
+        cd "{libdir}"
+        zip --recurse-paths "{zipfile}" * --exclude "*.pyc"
+        '''.format(
+            zipfile=zipfile, 
+            codedir=codedir, 
+            libdir=libdir,
+            ))
     with open(zipfile) as z:
         pp.pprint(client.update_function_code(
             ZipFile=z.read(),
             FunctionName='LambdaMLM',
             ))
-    local('rm "{zipfile}"'.format(zipfile=zipfile))
+        local('rm "{zipfile}"'.format(zipfile=zipfile))
+
+def setup_virtualenv():
+    local('''
+        cd "{basepath}"
+        virtualenv --no-site-packages --distribute .env \
+            && source .env/bin/activate \
+            && pip install -r requirements.txt
+        '''.format(
+            basepath=basepath,
+            ))
