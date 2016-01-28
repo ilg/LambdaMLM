916f84beb59e778bdd0e1f8785f2bfe714c1643b
Isaac Greenspan <ilg@2718.us>
2016-01-27 23:08:18 -0600

    Move list receive/send mechanics into the List object.
    Renamed ListConfiguration -> List; listcfg -> listobj.

--- lambda/lambda.py
+++ lambda/lambda.py
@@ -3,13 +3,9 @@ from __future__ import print_function
 from sestools import email_message_from_s3_bucket, event_msg_is_to_command, msg_get_header, recipient_destination_overlap
 from cnc import handle_command
 
-import boto3
-
-ses = boto3.client('ses')
-
 from config import email_bucket
 
-from listcfg import ListConfiguration
+from listobj import List
 
 def lambda_handler(event, context):
     with email_message_from_s3_bucket(event, email_bucket) as msg:
@@ -20,23 +16,8 @@ def lambda_handler(event, context):
             handle_command(command_address, msg)
             return
         
-        del msg['DKIM-Signature']
         print('Message from {}.'.format(msg_get_header(msg, 'from')))
         # See if the message was sent to any known lists.
-        for addr in recipient_destination_overlap(event):
-            print('Looking for list {}...'.format(addr))
-            try:
-                cfg = List(addr)
-            except ValueError:
-                continue
-            print('Found list {}.'.format(addr))
-            del msg['Sender']
-            msg['Sender'] = cfg.address
-            for user, flags in cfg.config['users'].iteritems():
-                print('> Sending to user {}.'.format(user))
-                ses.send_raw_email(
-                        Source=cfg.address,
-                        Destinations=[ user, ],
-                        RawMessage={ 'Data': msg.as_string(), },
-                        )
-            
+        for l in List.lists_for_addresses(recipient_destination_overlap(event)):
+            print('Sending to list {}.'.format(l.address))
+            l.send(msg)
--- lambda/listobj.py
+++ lambda/listobj.py
@@ -10,6 +10,7 @@ name_regex = re.compile(r'^[a-z0-9-]+$')
 import boto3
 
 s3 = boto3.client('s3')
+ses = boto3.client('ses')
 
 from config import config_bucket
 
@@ -39,4 +40,35 @@ class List:
             raise e
         self.config = yaml.load(config_response['Body'])
         print('Loaded list {} configuration: {}'.format(self.address, self.config))
+
+    def send(self, msg):
+        # TODO: check if the list allows messages from this message's sender
+        # TODO: check if the list might allow messages from this message's sender with moderator approval
+
+        # Strip out any exising DKIM signature.
+        del msg['DKIM-Signature']
+
+        del msg['Sender']
+        msg['Sender'] = self.address
+        del msg['Return-path']
+        msg['Return-path'] = self.address  # TODO: VERP?
+        # TODO: subject tagging
+        # TODO: body footer
+        # TODO (maybe): batch sends
+        for user, flags in self.config['users'].iteritems():
+            # TODO: skip vacation users, maybe bouncing users
+            # TODO: skip sending back to the sender unless echopost is set
+            print('> Sending to user {}.'.format(user))
+            ses.send_raw_email(
+                    Source=self.address,
+                    Destinations=[ user, ],
+                    RawMessage={ 'Data': msg.as_string(), },
+                    )
             
+    @classmethod
+    def lists_for_addresses(cls, addresses):
+        for a in addresses:
+            try:
+                yield cls(a)
+            except ValueError:
+                continue
