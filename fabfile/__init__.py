4f5dec9f4011872105779f8e836a6659dd5bfbe9
Isaac Greenspan <ilg@2718.us>
2016-03-17 18:13:55 -0500

    Use fabfile lcd contexts.

--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -16,16 +16,10 @@ def update_lambda():
     zipfile = os.path.join(basepath, 'lambda.zip')
     codedir = os.path.join(basepath, 'lambda')
     libdir = os.path.join(basepath, '.env/lib/python2.7/site-packages')
-    local('''
-        cd "{codedir}"
-        zip --recurse-paths "{zipfile}" * --exclude "*.pyc"
-        cd "{libdir}"
-        zip --recurse-paths "{zipfile}" * --exclude "*.pyc" "boto*" "pip*" "docutils*" "setuptools*" "wheel*" "pkg_resources*" "*.dist-info/*"
-        '''.format(
-            zipfile=zipfile, 
-            codedir=codedir, 
-            libdir=libdir,
-            ))
+    with lcd(codedir):
+        local('zip --recurse-paths "{zipfile}" * --exclude "*.pyc"'.format(zipfile=zipfile))
+    with lcd(libdir):
+        local('zip --recurse-paths "{zipfile}" * --exclude "*.pyc" "boto*" "pip*" "docutils*" "setuptools*" "wheel*" "pkg_resources*" "*.dist-info/*"'.format(zipfile=zipfile))
     with open(zipfile) as z:
         pp.pprint(client.update_function_code(
             ZipFile=z.read(),
@@ -34,11 +28,11 @@ def update_lambda():
         local('rm "{zipfile}"'.format(zipfile=zipfile))
 
 def setup_virtualenv():
-    local('''
-        cd "{basepath}"
-        virtualenv --no-site-packages --distribute .env \
-            && source .env/bin/activate \
-            && pip install -r requirements.txt
-        '''.format(
-            basepath=basepath,
-            ))
+    with lcd(basepath):
+        local('''
+            virtualenv --no-site-packages --distribute .env \
+                && source .env/bin/activate \
+                && pip install -r requirements.txt
+            '''.format(
+                basepath=basepath,
+                ))
